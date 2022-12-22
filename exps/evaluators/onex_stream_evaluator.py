478c81a47ca28d20c7d4f5c9974aef79f0e49e92
Anas Benalla <anas.benalla@asygn.com>
2022-12-22 17:40:39 +0100

    fix_bug_evaluator

--- exps/evaluators/onex_stream_evaluator.py
+++ exps/evaluators/onex_stream_evaluator.py
@@ -189,6 +189,8 @@ class ONEX_COCOEvaluator:
                 label = self.dataloader.dataset.class_ids[int(cls[ind])]
 
                 #if int(img_id) in [15060,15061]:
+                # max_ids = max([x['id'] for x in eself.dataloader.dataset.coco.dataset["images"]])
+                #if int(img_id) in [max_ids -1, max_ids]: 
                 if int(img_id) in [34332,34333]:
                     continue
                 elif self.dataloader.dataset.coco.dataset['images'][int(img_id+1)]['fid'] == 0:
